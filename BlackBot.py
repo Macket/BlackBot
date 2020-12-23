b0933df46b08208f0c0c19cf489a15525de8e427
Makeev Ivan <makeev@phystech.edu>
2020-12-23 16:26:10 +0300

    created BlackBot class and divided code into BlackBot.py and main.py

--- BlackBot.py
+++ BlackBot.py
@@ -1,213 +1,191 @@
 import pywaves as pw
 import datetime
-import time
 import os
-import sys
+import configparser
 import random
-try:
-	import configparser
-except ImportError:
-	import ConfigParser as configparser
+from colors import *
 
 
-COLOR_RESET = "\033[0;0m"
-COLOR_GREEN = "\033[0;32m"
-COLOR_RED = "\033[1;31m"
-COLOR_BLUE = "\033[1;34m"
-COLOR_WHITE = "\033[1;37m"
+class BlackBot:
+    def __init__(self):
+        self.log_file = "grid.log"
 
+        # main
+        self.node = "https://nodes.wavesnodes.com"
+        self.chain = "mainnet"
+        self.matcher = "https://matcher.waves.exchange"
+        self.order_fee = int(0.003 * 10 ** 8)
+        self.order_lifetime = 29 * 86400  # 29 days
 
-def log(msg):
-    timestamp = datetime.datetime.utcnow().strftime("%b %d %Y %H:%M:%S UTC")
-    s = "[%s] %s:%s %s" % (timestamp, COLOR_WHITE, COLOR_RESET, msg)
-    print(s)
-    try:
-        f = open(LOGFILE, "a")
-        f.write(s + "\n")
-        f.close()
-    except:
-        pass
+        # account
+        self.private_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        self.wallet = None
 
+        # market
+        self.amount_asset_id = "WAVES"
+        self.price_asset_id = "DG2xFkPdDwKUoBkzGAhQtLpSGzfXLiCYPEzeKH2Ad24p"  # USDN
+        self.asset_pair = pw.AssetPair(pw.Asset(self.amount_asset_id), pw.Asset(self.price_asset_id))
 
-def grid_price(level):
-    return int(basePrice * (1 + INTERVAL) ** (level - GRID_LEVELS / 2))
+        # grid
+        self.interval = 0.005
+        self.tranche_size = 150000000
+        self.grid_levels = 20
+        self.grid = ["-"] * self.grid_levels
+        self.flexibility = 0
+        self.type = "SYMMETRIC"
+        self.base = "LAST"
+        self.base_price = 0
 
+        # logging
+        self.logfile = "grid.log"
 
-def place_order(order_type, level):
-    if 0 <= level < GRID_LEVELS and (grid[level] == "" or grid[level] == "-"):
-        price = grid_price(level)
-        price = int(price / 100) * 100
-        price = round(price / 10 ** (PAIR.asset2.decimals + (PAIR.asset2.decimals - PAIR.asset1.decimals)), 8)
-        price = float(str(price))
+    def log(self, msg):
+        timestamp = datetime.datetime.utcnow().strftime("%b %d %Y %H:%M:%S UTC")
+        s = "[%s] %s:%s %s" % (timestamp, COLOR_WHITE, COLOR_RESET, msg)
+        print(s)
         try:
-            balance_amount, balance_price = BLACKBOT.tradableBalance(PAIR)
-            tranche_size = int(TRANCHE_SIZE * (1 - (FLEXIBILITY / float(200)) + (random.random() * FLEXIBILITY / float(100))))
-            if order_type == "buy" and balance_price >= (tranche_size * price / 10 ** (PAIR.asset2.decimals + (PAIR.asset2.decimals - PAIR.asset1.decimals))):
-                o = BLACKBOT.buy(PAIR, tranche_size, price, maxLifetime=ORDER_LIFETIME, matcherFee=ORDER_FEE)
-            elif order_type == "sell":# and balance_amount >= (tranche_size * price / 10 ** (PAIR.asset2.decimals + (PAIR.asset2.decimals - PAIR.asset1.decimals))):
-                #price -= 1
-                o = BLACKBOT.sell(PAIR, tranche_size, price, maxLifetime=ORDER_LIFETIME, matcherFee=ORDER_FEE)
-            id = o.orderId
-            log(">> [%03d] %s%-4s order  %18.*f%s" % (level, COLOR_GREEN if order_type == "buy" else COLOR_RED, order_type.upper(), PAIR.asset2.decimals, price, COLOR_RESET))
+            f = open(self.logfile, "a")
+            f.write(s + "\n")
+            f.close()
         except:
-            id = ""
-        grid[level] = id
+            pass
 
+    def read_config(self, cfg_file):
+        if not os.path.isfile(cfg_file):
+            self.log("Missing config file")
+            self.log("Exiting.")
+            exit(1)
 
-def get_last_price():
-    try:
-        last_trade_price = int(float(PAIR.trades(1)[0]['price']) * 10 ** (PAIR.asset2.decimals + (PAIR.asset2.decimals - PAIR.asset1.decimals)))
-    except:
-        last_trade_price = 0
-    return last_trade_price
+        try:
+            config = configparser.RawConfigParser()
+            config.read(cfg_file)
 
+            # main
+            self.node = config.get('main', 'node')
+            self.chain = config.get('main', 'network')
+            self.matcher = config.get('main', 'matcher')
+            self.order_fee = config.getint('main', 'order_fee')
+            self.order_lifetime = config.getint('main', 'order_lifetime')
 
-CMD = ""
-CFG_FILE = ""
+            # account
+            self.private_key = config.get('account', 'private_key')
+            self.wallet = pw.Address(privateKey=self.private_key)
 
-if len(sys.argv) >= 2:
-    CFG_FILE = sys.argv[1]
+            # market
+            self.amount_asset_id = config.get('market', 'amount_asset')
+            self.price_asset_id = config.get('market', 'price_asset')
+            self.asset_pair = pw.AssetPair(pw.Asset(self.amount_asset_id), pw.Asset(self.price_asset_id))
 
-if len(sys.argv) == 3:
-    CMD = sys.argv[2].upper()
+            # grid
+            self.interval = config.getfloat('grid', 'interval')
+            self.tranche_size = config.getint('grid', 'tranche_size')
+            self.grid_levels = config.getint('grid', 'grid_levels')
+            self.grid = ["-"] * self.grid_levels
+            self.flexibility = config.getint('grid', 'flexibility')
+            self.base = config.get('grid', 'base').upper()
+            self.type = config.get('grid', 'type').upper()
+            self.base_price = self.get_base_price()
 
-if not os.path.isfile(CFG_FILE):
-    log("Missing config file")
-    log("Exiting.")
-    exit(1)
+            # logging
+            self.logfile = config.get('logging', 'logfile')
 
-# parse config file
-try:
-    log("%sReading config file '%s'" % (COLOR_RESET, CFG_FILE))
-    config = configparser.RawConfigParser()
-    config.read(CFG_FILE)
+            self.log("Config file '{0}' has been read".format(cfg_file))
+            self.log("-" * 80)
+            self.log("          Address : %s" % self.wallet.address)
+            self.log("  Amount Asset ID : %s" % self.amount_asset_id)
+            self.log("   Price Asset ID : %s" % self.price_asset_id)
+            self.log("-" * 80)
+            self.log("")
 
-    NODE = config.get('main', 'node')
-    MATCHER = config.get('main', 'matcher')
-    ORDER_FEE = config.getint('main', 'order_fee')
-    ORDER_LIFETIME = config.getint('main', 'order_lifetime')
+        except OSError:
+            self.log("Error reading config file")
+            self.log("Exiting.")
+            exit(1)
 
-    PRIVATE_KEY = config.get('account', 'private_key')
-    amountAssetID = config.get('market', 'amount_asset')
-    priceAssetID = config.get('market', 'price_asset')
+    def get_last_price(self):
+        try:
+            last_trade_price = int(float(self.asset_pair.last()) * 10 ** (
+                    self.asset_pair.asset2.decimals + (
+                        self.asset_pair.asset2.decimals - self.asset_pair.asset1.decimals)))
+        except:
+            last_trade_price = 0
+        return last_trade_price
 
-    INTERVAL = config.getfloat('grid', 'interval')
-    TRANCHE_SIZE = config.getint('grid', 'tranche_size')
-    FLEXIBILITY = config.getint('grid', 'flexibility')
-    GRID_LEVELS = config.getint('grid', 'grid_levels')
-    GRID_BASE = config.get('grid', 'base').upper()
-    GRID_TYPE = config.get('grid', 'type').upper()
+    def get_base_price(self):
+        base_price = 0
+        try:
+            if self.base.isdigit():
+                base_price = int(self.base)
+            elif self.base == "LAST":
+                base_price = self.get_last_price()
+            elif self.base == "BID":
+                base_price = self.asset_pair.orderbook()['bids'][0]['price']
+            elif self.base == "ASK":
+                base_price = self.asset_pair.orderbook()['asks'][0]['price']
+        except:
+            base_price = 0
+        if base_price == 0:
+            self.log("Invalid BASE price")
+            self.log("Exiting.")
+            exit(1)
+        return base_price
 
-    LOGFILE = config.get('logging', 'logfile')
+    def get_level_price(self, level):
+        return int(self.base_price * (1 + self.interval) ** (level - self.grid_levels / 2))
 
-    BLACKBOT = pw.Address(privateKey=PRIVATE_KEY)
+    def init_grid(self, base_level):
+        self.log("Grid initialisation [base price : %.*f]" % (
+            self.asset_pair.asset2.decimals, float(self.base_price) / 10 ** self.asset_pair.asset2.decimals))
+        self.log("Grid initialisation [base price : %.*f]" % (
+            self.asset_pair.asset2.decimals,
+            float(self.base_price) / 10 ** (self.asset_pair.asset2.decimals +
+                                            (self.asset_pair.asset2.decimals - self.asset_pair.asset1.decimals))))
 
-    log("-" * 80)
-    log("          Address : %s" % BLACKBOT.address)
-    log("  Amount Asset ID : %s" % amountAssetID)
-    log("   Price Asset ID : %s" % priceAssetID)
-    log("-" * 80)
-    log("")
-except:
-    log("Error reading config file")
-    log("Exiting.")
-    exit(1)
+        if self.type == "SYMMETRIC" or self.type == "BIDS":
+            for n in range(base_level - 1, -1, -1):
+                self.buy(n)
+        if self.type == "SYMMETRIC" or self.type == "ASKS":
+            for n in range(base_level + 1, self.grid_levels):
+                self.sell(n)
 
-pw.setNode(NODE, "mainnet")
-pw.setMatcher(MATCHER)
-PAIR = pw.AssetPair(pw.Asset(amountAssetID), pw.Asset(priceAssetID))
+    def buy(self, level):
+        if 0 <= level < self.grid_levels and (self.grid[level] == "" or self.grid[level] == "-"):
+            price = self.get_level_price(level)
+            price = int(price / 100) * 100
+            price = round(price / 10 ** (self.asset_pair.asset2.decimals +
+                                         (self.asset_pair.asset2.decimals - self.asset_pair.asset1.decimals)), 8)
+            price = float(str(price))
+            try:
+                tranche_size = int(
+                    self.tranche_size * (1 - (self.flexibility / float(200)) + (
+                                random.random() * self.flexibility / float(100))))
+                order = self.wallet.buy(self.asset_pair, tranche_size, price,
+                                        matcherFee=self.order_fee, maxLifetime=self.order_lifetime)
+                order_id = order.orderId
+                self.log(">> [%03d] %s%-4s order  %18.*f%s" % (
+                    level, COLOR_GREEN, 'BUY', self.asset_pair.asset2.decimals, price, COLOR_RESET))
+            except:
+                order_id = ""
+            self.grid[level] = order_id
 
-# grid list with GRID_LEVELS items. item n is the ID of the order placed at the price calculated with this formula
-# price = int(basePrice * (1 + INTERVAL) ** (n - GRID_LEVELS / 2))
+    def sell(self, level):
+        if 0 <= level < self.grid_levels and (self.grid[level] == "" or self.grid[level] == "-"):
+            price = self.get_level_price(level)
+            price = int(price / 100) * 100
+            price = round(price / 10 ** (self.asset_pair.asset2.decimals +
+                                         (self.asset_pair.asset2.decimals - self.asset_pair.asset1.decimals)), 8)
+            price = float(str(price))
+            try:
+                tranche_size = int(
+                    self.tranche_size * (1 - (self.flexibility / float(200)) + (
+                                random.random() * self.flexibility / float(100))))
 
-grid = ["-"] * GRID_LEVELS
+                order = self.wallet.sell(self.asset_pair, tranche_size, price,
+                                         maxLifetime=self.order_lifetime, matcherFee=self.order_fee)
+                order_id = order.orderId
+                self.log(">> [%03d] %s%-4s order  %18.*f%s" % (
+                    level, COLOR_RED, 'SELL', self.asset_pair.asset2.decimals, price, COLOR_RESET))
+            except:
+                order_id = ""
+            self.grid[level] = order_id
 
-log("Cancelling open orders...")
-
-# cancel all open orders on the specified pair
-BLACKBOT.cancelOpenOrders(PAIR)
-log("Deleting order history...")
-
-# delete order history on the specified pair
-BLACKBOT.deleteOrderHistory(PAIR)
-log("")
-
-# terminate if RESET argument is given on the command line
-if CMD == "RESET":
-    log("Exiting.")
-    exit(1)
-
-# initialize grid
-try:
-    if GRID_BASE.isdigit():
-        basePrice = int(GRID_BASE)
-    elif GRID_BASE == "LAST":
-        basePrice = get_last_price()
-    elif GRID_BASE == "BID":
-        basePrice = PAIR.orderbook()['bids'][0]['price']
-    elif GRID_BASE == "ASK":
-        basePrice = PAIR.orderbook()['asks'][0]['price']
-except:
-    basePrice = 0
-if basePrice == 0:
-    log("Invalid BASE price")
-    log("Exiting.")
-    exit(1)
-
-#log("Grid initialisation [base price : %.*f]" % (PAIR.asset2.decimals, float(basePrice) / 10 ** PAIR.asset2.decimals))
-log("Grid initialisation [base price : %.*f]" % (PAIR.asset2.decimals, float(basePrice) / 10 ** (PAIR.asset2.decimals + (PAIR.asset2.decimals - PAIR.asset1.decimals))))
-
-last_level = int(GRID_LEVELS / 2)
-
-if GRID_TYPE == "SYMMETRIC" or GRID_TYPE == "BIDS":
-    for n in range(last_level - 1, -1, -1):
-        place_order("buy", n)
-if GRID_TYPE == "SYMMETRIC" or GRID_TYPE == "ASKS":
-    for n in range(last_level + 1, GRID_LEVELS):
-        place_order("sell", n)
-
-# loop forever
-while True:
-    # attempt to retrieve order history from matcher
-    try:
-        history = BLACKBOT.getOrderHistory(PAIR)
-    except:
-        history = []
-
-    if history:
-        # loop through all grid levels
-        # first all ask levels from the lowest ask to the highest -> range(grid.index("") + 1, len(grid))
-        # then all bid levels from the highest to the lowest -> range(grid.index("") - 1, -1, -1)
-        for n in list(range(last_level + 1, len(grid))) + list(range(last_level - 1, -1, -1)):
-
-            # find the order with id == grid[n] in the history list
-
-            order = [item for item in history if item['id'] == grid[n]]
-            status = order[0].get("status") if order else ""
-            if status == "Filled":
-                BLACKBOT.deleteOrderHistory(PAIR)
-                last_price = get_last_price()
-                grid[n] = ""
-                last_level = n
-                filled_price = order[0].get("price")
-                filled_type = order[0].get("type")
-                log("## [%03d] %s%-4s Filled %18.*f%s" % (n, COLOR_BLUE, filled_type.upper(), PAIR.asset2.decimals, float(filled_price) / 10 ** (PAIR.asset2.decimals + (PAIR.asset2.decimals - PAIR.asset1.decimals)), COLOR_RESET))
-
-                if filled_type == "buy":
-                    if filled_price >= last_price:
-                        place_order("sell", n + 1)
-                    else:
-                        place_order("buy", n)
-                elif filled_type == "sell":
-                    if filled_price <= last_price:
-                        place_order("buy", n - 1)
-                    else:
-                        place_order("sell", n)
-            # attempt to place again orders for empty grid levels or cancelled orders
-            elif (status == "" or status == "Cancelled") and grid[n] != "-":
-                grid[n] = ""
-                if n > last_level:
-                    place_order("sell", n)
-                elif n < last_level:
-                    place_order("buy", n)
-    time.sleep(5)
---
+++ main.py
@@ -0,0 +1,94 @@
+import time
+import os
+import sys
+import pywaves as pw
+
+from colors import *
+from BlackBot import BlackBot
+
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+
+
+def main():
+    bot = BlackBot()
+
+    cfg_file = ""
+    if len(sys.argv) >= 2:
+        cfg_file = sys.argv[1]
+    if not os.path.isfile(cfg_file):
+        bot.log("Missing config file")
+        bot.log("Exiting.")
+        exit(1)
+
+    bot.read_config(cfg_file)
+    pw.setNode(node=bot.node, chain=bot.chain)
+    pw.setMatcher(node=bot.matcher)
+
+    # grid list with GRID_LEVELS items. item n is the ID of the order placed at the price calculated with this formula
+    # price = int(basePrice * (1 + INTERVAL) ** (n - GRID_LEVELS / 2))
+
+    bot.log("Cancelling open orders...")
+    bot.wallet.cancelOpenOrders(bot.asset_pair)  # cancel all open orders on the specified pair
+
+    bot.log("Deleting order history...")
+    bot.wallet.deleteOrderHistory(bot.asset_pair)  # delete order history on the specified pair
+    bot.log("")
+
+    last_level = int(bot.grid_levels / 2)
+
+    # loop forever
+    while True:
+        # attempt to retrieve order history from matcher
+        try:
+            history = bot.wallet.getOrderHistory(bot.asset_pair)
+        except:
+            history = []
+
+        if history:
+            # loop through all grid levels
+            # first all ask levels from the lowest ask to the highest -> range(grid.index("") + 1, len(grid))
+            # then all bid levels from the highest to the lowest -> range(grid.index("") - 1, -1, -1)
+            for n in list(range(last_level + 1, len(bot.grid))) + list(range(last_level - 1, -1, -1)):
+
+                # find the order with id == grid9*-+[n] in the history list
+
+                order = [item for item in history if item['id'] == bot.grid[n]]
+                status = order[0].get("status") if order else ""
+                if status == "Filled":
+                    bot.wallet.deleteOrderHistory(bot.asset_pair)
+                    # last_price = bot.get_last_price()
+                    bot.grid[n] = ""
+                    last_level = n
+                    last_price = bot.get_last_price()
+                    filled_price = order[0].get("price")
+                    filled_type = order[0].get("type")
+                    bot.log("## [%03d] %s%-4s Filled %18.*f%s" % (
+                        n, COLOR_BLUE, filled_type.upper(), bot.asset_pair.asset2.decimals,
+                        float(filled_price) / 10 ** (bot.asset_pair.asset2.decimals + (
+                                bot.asset_pair.asset2.decimals - bot.asset_pair.asset1.decimals)), COLOR_RESET))
+
+                    if filled_type == "buy":
+                        if filled_price >= last_price:
+                            bot.sell(n + 1)
+                        else:
+                            bot.buy(n)
+                    elif filled_type == "sell":
+                        if filled_price <= last_price:
+                            bot.buy(n - 1)
+                        else:
+                            bot.sell(n)
+                # attempt to place again orders for empty grid levels or cancelled orders
+                elif (status == "" or status == "Cancelled") and bot.grid[n] != "-":
+                    bot.grid[n] = ""
+                    if n > last_level:
+                        bot.sell(n)
+                    elif n < last_level:
+                        bot.buy(n)
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
